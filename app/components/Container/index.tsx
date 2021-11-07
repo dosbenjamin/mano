509d8a1a0e98fa7ad5ff749b533acca75700182a
Benjamin Dos Santos <dosbenjamin@icloud.com>
2021-11-07 18:46:38 +0100

    feat(Container): add component

---
+++ app/components/Container/index.tsx
@@ -0,0 +1,13 @@
+import { Stack, StackProps } from "@chakra-ui/react"
+
+type Props = StackProps & {}
+
+const Container = ({ h = "600px", w = "1024px", children, ...rest }: Props) => {
+  return (
+    <Stack h={h} w={w} color="black" bg="white" borderRadius="lg" padding="16" {...rest}>
+      {children}
+    </Stack>
+  )
+}
+
+export default Container
