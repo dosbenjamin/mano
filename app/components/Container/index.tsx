7ae4bccf4788ec0f268dc25e24e929fca98a69cb
Benjamin Dos Santos <dosbenjamin@icloud.com>
2021-11-09 21:56:19 +0100

    feat(App): minor changes

--- app/components/Container/index.tsx
+++ app/components/Container/index.tsx
@@ -1,10 +1,19 @@
 import { Stack, StackProps } from "@chakra-ui/react"
 
-type Props = StackProps & {}
+type Props = StackProps
 
 const Container = ({ h = "600px", w = "1024px", children, ...rest }: Props) => {
   return (
-    <Stack h={h} w={w} color="black" bg="white" borderRadius="lg" padding="16" {...rest}>
+    <Stack
+      h={h}
+      w={w}
+      color="black"
+      bg="white"
+      borderRadius="lg"
+      padding="16"
+      boxShadow="xl"
+      {...rest}
+    >
       {children}
     </Stack>
   )
--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import { Center, CenterProps } from "@chakra-ui/react"
 
 type Props = CenterProps & {
-  rest?: any
+  rest?: CenterProps
 }
 
 const Layout = ({ children, rest }: Props) => {
   return (
-    <Center h="100vh" w="100vw" bg="black" {...rest}>
+    <Center h="100vh" w="100vw" bg="red.100" {...rest}>
       {children}
     </Center>
   )
